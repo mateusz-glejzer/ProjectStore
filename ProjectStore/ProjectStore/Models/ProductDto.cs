77630f4db6b1dba11df40489b10ffeece1212ad7
Mateusz <papierekm@gmail.com>
2021-12-22 12:14:19 +0100

    images

--- ProjectStore/ProjectStore/Models/ProductDto.cs
+++ ProjectStore/ProjectStore/Models/ProductDto.cs
@@ -7,6 +7,6 @@
         public string Description { get; set; }
         public bool IsAvailable { get; set; }
         public decimal Price { get; set; }
-        //     public byte[] Image { get; set; }
+        public byte[] Image { get; set; }
     }
 }
