d53167880a021ca0fbbb605f36b739f2fac20422
Mateusz <papierekm@gmail.com>
2022-01-18 18:07:09 +0100

    edit Tests that delete product

--- ProjectStoreTests/ProductControllerTests.cs
+++ ProjectStoreTests/ProductControllerTests.cs
@@ -1,37 +1,129 @@
-using System;
-using Xunit;
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Authorization.Policy;
 using Microsoft.AspNetCore.Mvc.Testing;
-using ProjectStore;
-using System.Net.Http;
-using System.Linq;
 using Microsoft.EntityFrameworkCore;
-using ProjectStore.Entities;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using ProjectStore;
+using ProjectStore.Entities;
+using ProjectStore.Models;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
-namespace ProjectStoreTests
+namespace ProjectStoreBlazorTests
 {
-    public class ProductControllerTests: IClassFixture<WebApplicationFactory<Startup>>
+    public class ProductControllerTests : IClassFixture<WebApplicationFactory<Startup>>
     {
         private HttpClient _client;
+        WebApplicationFactory<Startup> _factory;
         public ProductControllerTests(WebApplicationFactory<Startup> factory)
         {
-            _client = factory
-                .WithWebHostBuilder(builder => {
+
+
+            _factory = factory
+                .WithWebHostBuilder(builder =>
+                {
 
                     builder.ConfigureServices(services =>
                     {
-                        var dbcontextOptions= services.SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<StoreDbContext>));
+                        var dbcontextOptions = services.SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<StoreDbContext>));
                         services.Remove(dbcontextOptions);
-                        services.AddDbContext<StoreDbContext>(options => options.UseInMemoryDatabase("StoreDb"));
+                        services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+                        services.AddMvc(option => option.Filters.Add(new FakeUserFilter()));
+                        services.AddEntityFrameworkInMemoryDatabase().AddDbContext<StoreDbContext>(options => options.UseInMemoryDatabase("StoreDb"));
                     });
-                    })
-                .CreateClient();
+                });
+            _client = _factory.CreateClient();
+                
         }
-        
-        [Fact]
-        public void GetAll_()
+        private void Seed(Product product) 
         {
+            var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+            var _dbContext = scope.ServiceProvider.GetService<StoreDbContext>();
+            _dbContext.Products.Add(product);
+            _dbContext.SaveChanges();
+        }
 
+        [Fact]
+        public async Task CreateProduct_WithValidModel_ReturnOkStatus()
+        {
+            //arange
+            var model = new ProductDto()
+            {
+                Name = "bacardi",
+                Description = "w Klubie ze szklanki",
+                IsAvailable = true,
+                Price = 420,
+
+            };
+            var json = JsonConvert.SerializeObject(model);
+            var httpContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+            //act
+            var response = await _client.PostAsync("Product/Add", httpContent);
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            
+
+        }
+        [Fact]
+        public async Task DeleteProduct_ForNonExistingProduct_ReturnsNotFound()
+        {   //arange
+            var productId = 997;
+            //act
+            var response = await _client.DeleteAsync($"Product/Delete/{productId}");
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+           
+        }
+        [Fact]
+        public async Task Delete_ForValidUser_ReturnsNoContent() 
+        {
+            //arrange
+           
+            var product = new Product
+            {
+                CreatedByUserId = 1,
+                Name = "Test"
+            };
+            //seed
+            Seed(product);
+            //act
+            var response = await _client.DeleteAsync($"Product/Delete/{product.CreatedByUserId}");
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+
+        }
+
+
+        [Fact]
+        public async void GetElementById_WithValidIdParameter_ReturnOkResult()
+        {
+            //arange
+            var product = new Product()
+            {
+                Id = 1,
+                Name = "testproduct1",
+                Description = "producttotest",
+                IsAvailable = true,
+                Price = 420,
+
+            };
+            //seed
+
+            Seed(product);
+            //act
+            var response = await _client.GetAsync($"Product/Get/{1}");
+           
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
         }
     }
 }
