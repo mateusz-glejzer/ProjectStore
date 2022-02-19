1364cb568ac87047a5361e34a1137edab2d57568
Mateusz <papierekm@gmail.com>
2022-02-19 13:23:24 +0100

    add BasketController with add,get,delete,change controllers

---
+++ ProjectStore/Controllers/BasketController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectStore.Models;
+using System.Threading.Tasks;
+
+namespace ProjectStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> AddToBasket(ProductDto product)
+        {
+            
+            return Ok();
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBasket()
+        {
+
+            return Ok();
+
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> DeleteFromBasket(int id)
+        {
+
+            return Ok();
+
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> ChangeQuantityInBasket(int id)
+        {
+
+            return Ok();
+
+        }
+
+
+
+
+    }
+}
